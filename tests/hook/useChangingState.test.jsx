5fbf551938c0a03d58f8644b834fb1075a1c0fac
tetthys <jeongwoo0830z@gmail.com>
2024-01-29 18:16:51 +0900

    add comment

--- tests/hook/useChangingState.test.jsx
+++ tests/hook/useChangingState.test.jsx
@@ -20,4 +20,23 @@ describe("useChangingState", () => {
 
     expect(result.current.data).toBe(2);
   });
+
+  /**
+   * All code that causes a change to a state of component or hook should be used inside 'act'
+   *
+   * Although it is just a funtion that only delays and does not change any state of component or hook like below
+   *
+   * await delay(ENSURE_TIMEOUT);
+   *
+   * It's a bit weird...
+   *
+   * But just accept it.
+   */
+  // it("get 2 after some seconds", async () => {
+  //   const { result } = renderHook(() => useChangingState());
+
+  //   await delay(ENSURE_TIMEOUT);
+
+  //   expect(result.current.data).toBe(2);
+  // });
 });
