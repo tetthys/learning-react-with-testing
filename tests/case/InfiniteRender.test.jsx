a4bcd219834329282a4d72665e7420880256f4c6
tetthys <jeongwoo0830z@gmail.com>
2024-01-29 17:50:54 +0900

    Add comments explaining infinite render in test files

--- tests/case/InfiniteRender.test.jsx
+++ tests/case/InfiniteRender.test.jsx
@@ -2,6 +2,9 @@ import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import InfiniteRender from "../../src/case/InfiniteRender";
 
+// I triggered infinite render on purpose to see how render works.
+// It doesn't end.
+
 describe.skip("InfiniteRender", () => {
   it("render", () => {
     const { getByTestId } = render(<InfiniteRender />);
--- tests/hook/useInfiniteRender.test.jsx
+++ tests/hook/useInfiniteRender.test.jsx
@@ -2,6 +2,9 @@ import { renderHook } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import useInfiniteRender from "../../src/hook/useInfiniteRender";
 
+// I triggered infinite render on purpose to see how renderHook works.
+// It doesn't end.
+
 describe.skip("useInfiniteRender", () => {
   it("render", () => {
     const { result } = renderHook(() => useInfiniteRender());
