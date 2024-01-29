2d4121e9881b073866b395406ddf93847fb89107
tetthys <jeongwoo0830z@gmail.com>
2024-01-29 16:42:35 +0900

    Skip tests for InfiniteRender and useInfiniteRender

--- tests/case/InfiniteRender.test.jsx
+++ tests/case/InfiniteRender.test.jsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import InfiniteRender from "../../src/case/InfiniteRender";
 
-describe("InfiniteRender", () => {
+describe.skip("InfiniteRender", () => {
   it("render", () => {
     const { getByTestId } = render(<InfiniteRender />);
     expect(getByTestId("rendered")).toHaveTextContent("1");
--- tests/hook/useInfiniteRender.test.jsx
+++ tests/hook/useInfiniteRender.test.jsx
@@ -2,7 +2,7 @@ import { renderHook } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import useInfiniteRender from "../../src/hook/useInfiniteRender";
 
-describe("useInfiniteRender", () => {
+describe.skip("useInfiniteRender", () => {
   it("render", () => {
     const { result } = renderHook(() => useInfiniteRender());
     expect(result.current).toBe(true);
